12df0cb65d2ed321b9ecaeadc636dde6246a35d2
Alexander Rettner <alexander.rettner@web.de>
2023-01-15 12:09:56 +0100

    Data Fetching on server side

--- src/ui/qute-app/pages/index.tsx
+++ src/ui/qute-app/pages/index.tsx
@@ -1,42 +1,42 @@
-import {useEffect, useState} from "react";
-import {deleteTask, getAllTasks} from "./TaskService";
-import {Task} from "./Task";
+import { useState } from "react";
+import { deleteTask, getAllTasks } from "./TaskService";
+import { Task } from "./Task";
 
-export default function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [isLoading, setLoading] = useState(false);
+const quarkusUrl = `http://localhost:8080`;
 
-  useEffect(() => {
-    setLoading(true);
-    getAllTasks()
-      .then((tasks) => {
-        setTasks(tasks)
-        setLoading(false)
-      })
-  }, []);
+export default function Home({data}) {
+    const [tasks, setTasks] = useState<Task[]>(data);
+    const [isLoading, setLoading] = useState(false);
 
-  const onDeleteTask = (key: string) => deleteTask(key)
-    .finally(() => {
-      setLoading(true);
-      getAllTasks()
-        .then((tasks) => {
-          setTasks(tasks)
-          setLoading(false)
+    const onDeleteTask = (key: string) => deleteTask(key)
+        .finally(() => {
+            setLoading(true);
+            getAllTasks()
+                .then((tasks) => {
+                    setTasks(tasks)
+                    setLoading(false)
+                })
         })
-    })
 
-  if (isLoading) return <p>Loading...</p>
-  if (!tasks) return <p>No tasks</p>
+    if (isLoading) return <p>Loading...</p>
+    if (!tasks) return <p>No tasks</p>
 
-  return (
-    <div>
-      <h1>Tasks</h1>
-      <ul>
-        {tasks.map(task =>
-          <li onClick={() => onDeleteTask(task.key)}
-              key={task.key}>{task.dueDate ? task.dueDate : ''}{task.dueDate && task.title ? ': ' : ''}{task.title ? task.title : ''}</li>
-        )}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Tasks</h1>
+            <ul>
+                {tasks.map(task =>
+                    <li onClick={() => onDeleteTask(task.key)}
+                        key={task.key}>{task.dueDate ? task.dueDate : ''}{task.dueDate && task.title ? ': ' : ''}{task.title ? task.title : ''}</li>
+                )}
+            </ul>
+        </div>
+    );
+}
+
+export async function getServerSideProps(): Promise<{ props: { data: Task[] } }> {
+    const response = await fetch(`${quarkusUrl}/tasks`)
+    const data: Task[] = await response.json();
+
+    return {props: {data}}
 }
